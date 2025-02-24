58bc2bbb9bba1fab5f568f2cb4f70773ef3e13a6
unknown <rushikeshlondhe942@gmail.com>
2025-02-25 01:12:04 +0530

    basic

---
+++ basicprogram.py
@@ -0,0 +1,93 @@
+
+
+# is_active = True
+# name = str(input("whats your name: "))
+# if name == "rishi":
+#   print("is_active") 
+
+# else:
+#   print("false")
+
+# my_string ='sapm eggs'
+# print(my_string)
+# my_string2 = 'spam eggs 2'
+# print(my_string2)
+
+# grammar = "dosen't"
+# print(grammar)
+# grammar2 = 'dosen\'t'
+# print(grammar2)
+
+
+# n = int(input("enter a digit: "))
+# if n % 10 == 0:
+#     print(True)
+# else:
+#     print(False)
+ # day 2//
+
+
+# x = int(input("plese enter the number"))
+# if x < 0:
+#     x = 0
+#     print('negative is change to zero')
+
+# elif x == 0:
+#     print("integer is alredy zero")
+
+# elif x == 1:
+#     print(-2s one")
+
+    # for staement
+
+# list1 = [ 'cat' , 'dog', 'elephent' ]
+
+# for w in list1:
+#     print(w, len(w))
+#3//
+
+# users  = {
+#     "rushi" : "active",
+#     "harshu" : "active",
+#     "abhi" : "inactive"
+# }
+# print(type(users))
+# print(users)
+# for user, status in users.copy().items():
+#     if status == 'inactive':
+#         delii users[user]
+
+# active_users = {}
+
+# for user, status in users.items():
+#     if status == "active":
+#         active_users[user] = status
+
+# print(users)
+# print(active_users)
+
+OAOAOA
+# # range function 
+
+OAOAOA# for i in range (0, 22, 2):
+#     print(i)
+OAOAOAOAOAOA
+# a = list(range(0, 22, 2))
+# type(a)
+
+# a = ["marry", "has", "a", "little", "lamb"]
+OAOAOA# for i in range(len(a)):
+#     print(i, a[i])
+
+##brake & continue
+
+# for num in range(1, 6):
+#     if num == 3:
+#         break
+#     print(num)
+
+
+# for i in range(1, 6):
+#     if i == 4:
+#         continue
+#     print(i)
