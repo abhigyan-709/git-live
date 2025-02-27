7ad197c4ca2b08ef7db605ade917bafb96c26c98
LostBoyAR <adityarajgupta9798@gmail.com>
2025-02-27 21:02:55 +0530

    remove unused Python files and scripts

--- remove.py
+++
@@ -1,8 +0,0 @@
-orig_list = [1,1, 2, 2, 3, 4, 4, 5,5, 6, 6]
-uniq_list = []
-for element in orig_list:
-    if element not in uniq_list:
-        uniq_list.append(element)
-
-print("Original list:", orig_list)
-print("List after removing duplicates:", uniq_list)
