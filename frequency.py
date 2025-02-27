5d07b13c5f0f8bfc4a4fef2176224edf8151ef65
Priyanshu Kumar <priyanshukumar7g@gmail.com>
2025-02-27 21:50:34 +0530

    Added frequency.py

---
+++ frequency.py
@@ -0,0 +1,12 @@
+def word_frequency(f):
+    sword= f.lower().split()
+    freq = {}
+    for word in sword:
+        word = word.strip(".,!?()[]{}:;\"'")
+        freq[word] = freq.get(word, 0) + 1
+    return freq
+text = input("Enter a string: ")
+freq_dict = word_frequency(text)
+for word, count in freq_dict.items():
+    print(f"{word}: {count}")
+ new content
