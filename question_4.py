756c7d18406abea273e703cfef647190ed374672
punitkotian <kotianpuneeth474@gmail.com>
2025-02-27 19:43:33 +0530

    question_4 implementation

---
+++ question_4.py
@@ -0,0 +1,5 @@
+# 4. write a python program to convert a list of tuples into a dictionary
+
+tuple_list = [('name','john'), ('age', 22), ('email', 'john@gmail.com')]
+dict = dict(tuple_list)
+print(dict)
